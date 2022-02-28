8f0516244d4607b87e9cecc5ed5e3e518d1348ae
LionyxML <rahul.juliato@gmail.com>
2022-02-27 21:52:19 -0300

    feat: adds models and simple implementation

---
+++ src/script.js
@@ -0,0 +1,40 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+]).then(startVideo);
+
+function startVideo() {
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia(
+      { video: true },
+      (stream) => (video.srcObject = stream),
+      (err) => console.log(err)
+    );
+  }
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: 800, height: 600 };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    const minProbability = 0.05;
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections, minProbability);
+  }, 100);
+});
